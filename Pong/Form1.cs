9113303b82d9c985ac59f79ea2d8667c22adbb65
roltik888 <dzgofs@gmail.com>
2026-04-15 09:34:58 +0200

    Update Form1.cs

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -5,6 +5,17 @@ namespace Pong
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 2000;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
